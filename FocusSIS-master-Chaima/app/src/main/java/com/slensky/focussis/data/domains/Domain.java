1ef02eb3161abf645d762a2fdfb9e385ef994ff2
emnaayedi <49609035+emnaayedi@users.noreply.github.com>
2021-10-08 18:50:21 +0100

    Delete FocusSIS-master-Chaima directory

--- FocusSIS-master-Chaima/app/src/main/java/com/slensky/focussis/data/domains/Domain.java
+++
@@ -1,7 +0,0 @@
-package com.slensky.focussis.data.domains;
-
-public interface Domain<K, M> {
-
-    void addMember(K key, M member);
-
-}
--- FocusSIS-master-Chaima/app/src/main/java/com/slensky/focussis/util/EventUtil.java
+++
@@ -1,8 +0,0 @@
-package com.slensky.focussis.util;
-
-/**
- * Created by slensky on 5/14/17.
- */
-
-public class EventUtil {
-}
